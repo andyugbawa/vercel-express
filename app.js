a0fbf14f88eb76e0090f236093820258b50a9dd3
andy <andyugbawa@gmail.com>
2025-04-02 03:49:39 +0100

    created all movies,  index,edit,show, app.js , created edit.ejs,new.ejs,show.ejs,index.ejs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Film = require("./models/movie");
+const methodOverride = require("method-override")
 
 
 
@@ -26,18 +27,51 @@ mongoose
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
 
 
 
-app.get("/",(req,res)=>{
-    res.render("home")
-})
-
-// app.get("/",async(req,res)=>{
-//     const movies = await Film.find({})
-//     res.render("movie/index",{movies})
+// app.get("/",(req,res)=>{
+//     res.render("home")
 // })
 
+app.get("/movie",async(req,res)=>{
+    const movies = await Film.find({})
+    res.render("movie/index",{movies})
+});
+
+app.get("/movie/new",(req,res)=>{
+  res.render("movie/new")
+})
+
+app.post("/movie",async(req,res)=>{
+  const movie = new Film(req.body.movie)
+  await movie.save()
+  res.redirect(`/movie/${movie._id}`)
+})
+
+app.get("/movie/:id",async(req,res)=>{
+  const movie = await Film.findById(req.params.id)
+  res.render("movie/show",{movie})
+})
+
+app.get("/movie/:id/edit",async(req,res)=>{
+  const movie = await Film.findById(req.params.id)
+  res.render("movie/edit",{movie})
+});
+
+app.put("/movie/:id",async(req,res)=>{
+  const{id}=req.params
+  const movie = await Film.findByIdAndUpdate(id, {...req.body.movie})
+  res.redirect(`/movie/${movie._id}`)
+});
+
+app.delete("/movie/:id",async(req,res)=>{
+  const{id}=req.params
+   await Film.findByIdAndDelete(id)
+  res.redirect("/movie")
+})
 
 
 app.listen(4001,()=>{
