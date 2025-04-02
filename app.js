211a6860f93f60c5a2879b62be4740a18b08834f
andy <andyugbawa@gmail.com>
2025-04-02 05:44:18 +0100

    test 5

--- app.js
+++ app.js
@@ -42,36 +42,37 @@ app.get("/",async(req,res)=>{
 });
 
 app.get("/new",(req,res)=>{
-  res.render("movie/new")
+  // res.render("movie/new")
+  res.send("New")
 })
 
-app.post("/movie",async(req,res)=>{
-  const movie = new Film(req.body.movie)
-  await movie.save()
-  res.redirect(`/movie/${movie._id}`)
-})
+// app.post("/movie",async(req,res)=>{
+//   const movie = new Film(req.body.movie)
+//   await movie.save()
+//   res.redirect(`/movie/${movie._id}`)
+// })
 
-app.get("/movie/:id",async(req,res)=>{
-  const movie = await Film.findById(req.params.id)
-  res.render("movie/show",{movie})
-})
+// app.get("/movie/:id",async(req,res)=>{
+//   const movie = await Film.findById(req.params.id)
+//   res.render("movie/show",{movie})
+// })
 
-app.get("/movie/:id/edit",async(req,res)=>{
-  const movie = await Film.findById(req.params.id)
-  res.render("movie/edit",{movie})
-});
+// app.get("/movie/:id/edit",async(req,res)=>{
+//   const movie = await Film.findById(req.params.id)
+//   res.render("movie/edit",{movie})
+// });
 
-app.put("/movie/:id",async(req,res)=>{
-  const{id}=req.params
-  const movie = await Film.findByIdAndUpdate(id, {...req.body.movie})
-  res.redirect(`/movie/${movie._id}`)
-});
+// app.put("/movie/:id",async(req,res)=>{
+//   const{id}=req.params
+//   const movie = await Film.findByIdAndUpdate(id, {...req.body.movie})
+//   res.redirect(`/movie/${movie._id}`)
+// });
 
-app.delete("/movie/:id",async(req,res)=>{
-  const{id}=req.params
-   await Film.findByIdAndDelete(id)
-  res.redirect("/")
-})
+// app.delete("/movie/:id",async(req,res)=>{
+//   const{id}=req.params
+//    await Film.findByIdAndDelete(id)
+//   res.redirect("/")
+// })
 
 
 app.listen(4001,()=>{
