426fdedc0b58c06395d5106ce244ad4320110a18
andy <andyugbawa@gmail.com>
2025-04-15 03:24:35 +0100

    created movie routes

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-// console.log("MONGO_URI:", process.env.MONGO_URI);
-
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -13,31 +11,21 @@ const {movieSchema,reviewSchema}=require("./schema.js")
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 
-
-
-
- 
+const movies = require("./routes/movies");
 
 
 const MONGO_URI = process.env.VERCEL_ENV === 'production'
 ? process.env.MONGO_URI_PROD
 : process.env.MONGO_URI_DEV;
-
-
  
 if (!MONGO_URI) {
   console.error("❌ MONGO_URI is missing in environment variables!");
   process.exit(1);
 }
-
-
-
 console.log("Using Mongo URI:", MONGO_URI);
 
-  
 mongoose.connect(MONGO_URI, {
   dbName: "moviestatetwo",
-
 })
 .then(() => console.log("✅ Connected to MongoDB successfully!"))
 .catch(err => {
@@ -45,32 +33,12 @@ mongoose.connect(MONGO_URI, {
   process.exit(1);
 });
 
-
-
-
-
-
-
-
-
-
 const absolutepath = path.join(__dirname, "./public");
 app.use(express.static(absolutepath));
 app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(methodOverride("_method"))
-
-const validateMovie = (req,res,next)=>{
- 
-  const {error} = movieSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el=>el.message).join(",")
-    throw new ExpressError(msg,400)
-  }else{
-    next();
-  }
-}
 
 const validateReview= (req,res,next)=>{
   const {error} =reviewSchema.validate(req.body)
@@ -82,54 +50,7 @@ const validateReview= (req,res,next)=>{
   }
 }
 
-// app.get("/",(req,res)=>{
-//     res.render("home")
-// })
-
-app.get("/",catchAsync(async(req,res)=>{
-    const movies = await Film.find({})
-    res.render("movie/index",{movies})
-    // res.render("movie/index")
-}));
-
-app.get("/movie/new",(req,res)=>{
-  res.render("movie/new")
-  // res.send("New")
-})
-
-app.post("/movie",validateMovie,catchAsync(async(req,res,next)=>{
-  // if(!req.body.movie) throw new ExpressError("Invalid movie Data", 404)
-    const movie = new Film(req.body.movie)
-    await movie.save()
-    res.redirect(`/movie/${movie._id}`)
-
- 
-}))
-
-
-
-
-app.get("/movie/:id",catchAsync(async(req,res)=>{
-  const movie = await Film.findById(req.params.id).populate("reviews")
-  res.render("movie/show",{movie})
-}))
-
-app.get("/movie/:id/edit",catchAsync(async(req,res)=>{
-  const movie = await Film.findById(req.params.id)
-  res.render("movie/edit",{movie})
-}));
-
-app.put("/movie/:id",validateMovie,catchAsync(async(req,res)=>{
-  const{id}=req.params
-  const movie = await Film.findByIdAndUpdate(id, {...req.body.movie})
-  res.redirect(`/movie/${movie._id}`)
-}));
-
-app.delete("/movie/:id",catchAsync(async(req,res)=>{
-  const{id}=req.params
-   await Film.findByIdAndDelete(id)
-  res.redirect("/")
-}));
+app.use("/movie", movies)
 
 app.post("/movie/:id/review",validateReview,catchAsync(async(req,res)=>{
    const movie = await Film.findById(req.params.id)
@@ -157,7 +78,6 @@ app.use((err,req,res,next)=>{
   res.status (statusCode).render("error",{err});
 
 })
-
 
 app.listen(4001,()=>{
     console.log("APP RUNING ON 4001")
---
+++ routes/movies.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const Film = require("../models/movie");
+const {movieSchema} = require("../schema.js");
+
+const validateMovie = (req,res,next)=>{
+  const {error} = movieSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el=>el.message).join(",")
+    throw new ExpressError(msg,400)
+  }else{
+    next();
+  }
+}
+
+router.get("/",catchAsync(async(req,res)=>{
+    const movies = await Film.find({})
+    res.render("movie/index",{movies})
+}));
+
+router.get("/new",(req,res)=>{
+  res.render("movie/new")
+})
+
+router.post("/",validateMovie,catchAsync(async(req,res,next)=>{
+    const movie = new Film(req.body.movie)
+    await movie.save()
+    res.redirect(`/movie/${movie._id}`)
+}))
+
+router.get("/:id",catchAsync(async(req,res)=>{
+  const movie = await Film.findById(req.params.id).populate("reviews")
+  res.render("movie/show",{movie})
+}))
+
+router.get("/:id/edit",catchAsync(async(req,res)=>{
+  const movie = await Film.findById(req.params.id)
+  res.render("movie/edit",{movie})
+}));
+
+router.put("/:id",validateMovie,catchAsync(async(req,res)=>{
+  const{id}=req.params
+  const movie = await Film.findByIdAndUpdate(id, {...req.body.movie})
+  res.redirect(`/movie/${movie._id}`)
+}));
+
+router.delete("/:id",catchAsync(async(req,res)=>{
+  const{id}=req.params
+   await Film.findByIdAndDelete(id)
+  res.redirect("/")
+}));
+
+module.exports = router;
