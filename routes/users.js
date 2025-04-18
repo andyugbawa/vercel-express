f1cbd994660d3faf42a687e4ca31656359f55f0f
andy <andyugbawa@gmail.com>
2025-04-18 21:02:36 +0100

    created a logout route in users.js

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,14 @@ router.post("/login",passport.authenticate("local",{failureFlash:true,failureRed
  res.redirect("/movie")
 });
 
+router.get("/logout", (req, res, next) => {
+   req.logout(function(err) {
+       if (err) { return next(err); }
+       req.flash("success", "Goodbye");
+       res.redirect("/movie");
+   });
+});
+
+
+
 module.exports = router;
