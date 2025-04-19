4cfc0f10dda8d63f3b695b37b261aec322510317
andy <andyugbawa@gmail.com>
2025-04-19 20:00:58 +0100

    fixing registeredUser in  register route

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,17 @@ router.get("/register",(req,res)=>{
    res.render("users/register")
 })
 
-router.post("/register",catchAsync(async(req,res)=>{
+router.post("/register",catchAsync(async(req,res,next)=>{
    try{
       const {email,username,password} = req.body;
       const user = new User({email,username})
       const registeredUser = await User.register(user,password);
-      console.log(registeredUser);
-      req.flash("success","WELCOME TO MOVIES")
-      res.redirect("/movie")
+      req.login(registeredUser,err=>{
+         if(err)return next(err)
+            console.log(registeredUser);
+            req.flash("success","WELCOME TO MOVIES")
+            res.redirect("/movie")
+      })
    }catch(e){
       req.flash("error",e.message);
       res.redirect("/register")
