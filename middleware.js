0448fbcc072c185ec1329918457afc028171bb83
andy <andyugbawa@gmail.com>
2025-04-19 13:15:13 +0100

    created a isLoggedIn middleware for the new routes,editroutes,sshow page routes and delete route

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-module.exports. isLoggedIn = (req,res,next)=>{
+module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.flash("error","You Must be Signed In First");
        return res.redirect("/login")
